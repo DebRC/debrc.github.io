abbe3d99bd9a517f1669ebfc1a9f713342d8d215
DebRC <roychowdhurydebdoot@gmail.com>
2023-03-28 19:56:20 +0530

    DebRC Logo

---
+++ lib/widgets/logo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio_site/helpers/screensize.dart';
+
+class Logo extends StatelessWidget {
+  final double width;
+  final ScrollController scrollController;
+  const Logo({required this.width, required this.scrollController, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => scrollController.animateTo(
+          scrollController.position.minScrollExtent,
+          duration: const Duration(milliseconds: 700),
+          curve: Curves.easeInOut),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Text("DebRC",
+            style: GoogleFonts.getFont('Delius',
+                color: Colors.white,
+                fontSize: width >= ScreenSize.xlg
+                    ? width * 0.02
+                    : ScreenSize.xlg * 0.02)),
+      ),
+    );
+  }
+}
