0e082927adb261e78e3c65cf62958600f8bfaad0
DebRC <roychowdhurydebdoot@gmail.com>
2023-03-28 20:53:52 +0530

    Fixed Education

---
+++ lib/helpers/education_list.dart
@@ -0,0 +1,26 @@
+import 'package:portfolio_site/models/education.dart';
+
+class EducationList {
+  static List<Education> educationList = [
+    Education(
+        name: "Govt. College of Engineering and Ceramic Technology",
+        degree: "Bachelor of Technology",
+        discipline: "Computer Science and Engineering",
+        period: "2019-2023"),
+    Education(
+        name: "Govt. College of Engineering and Ceramic Technology",
+        degree: "Bachelor of Technology",
+        discipline: "Computer Science and Engineering",
+        period: "2019-2023"),
+    Education(
+        name: "Govt. College of Engineering and Ceramic Technology",
+        degree: "Bachelor of Technology",
+        discipline: "Computer Science and Engineering",
+        period: "2019-2023"),
+    Education(
+        name: "Govt. College of Engineering and Ceramic Technology",
+        degree: "Bachelor of Technology",
+        discipline: "Computer Science and Engineering",
+        period: "2019-2023"),
+  ];
+}
---
+++ lib/models/education.dart
@@ -0,0 +1,13 @@
+class Education {
+  final String name;
+  final String degree;
+  final String discipline;
+  final String period;
+
+  Education({
+    required this.name,
+    required this.degree,
+    required this.discipline,
+    required this.period,
+  });
+}
