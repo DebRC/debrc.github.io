ab4f60c7ff85d1eea6092b4be8c20975dbbd66bb
DebRC <roychowdhurydebdoot@gmail.com>
2023-03-28 19:45:47 +0530

    Added Container for Upper Part

---
+++ lib/widgets/upper_container.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_site/helpers/colors.dart';
+import 'package:portfolio_site/helpers/screensize.dart';
+import 'package:portfolio_site/widgets/description.dart';
+import 'package:portfolio_site/widgets/profile_image.dart';
+
+class UpperContainer extends StatelessWidget {
+  final double width;
+  const UpperContainer({required this.width, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      color: CustomColors.brightBackground,
+      padding: const EdgeInsets.only(bottom: 20),
+      child: LayoutBuilder(builder: (context, consraints) {
+        if (consraints.maxWidth >= ScreenSize.lg) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: width * 0.02),
+              Description(
+                isVertical: false,
+                width: width,
+              ),
+              const SizedBox(width: 20),
+              ProfileImage(
+                width: width,
+              )
+            ],
+          );
+        } else if (consraints.maxWidth < ScreenSize.lg &&
+            consraints.maxWidth >= ScreenSize.md) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: width * 0.07),
+              ProfileImage(width: (2 * width) - 0.16 * width),
+              SizedBox(height: 0.05 * width),
+              Description(
+                isVertical: true,
+                width: width,
+              ),
+            ],
+          );
+        } else {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: width * 0.07),
+              Center(child: ProfileImage(width: 2 * width)),
+              SizedBox(height: 0.05 * width),
+              Description(isVertical: true, width: width),
+            ],
+          );
+        }
+      }),
+    );
+  }
+}
