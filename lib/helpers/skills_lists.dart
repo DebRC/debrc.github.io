b04de5f3ae93e5130e0c4ff0ef7cea033092e36a
DebRC <roychowdhurydebdoot@gmail.com>
2023-03-29 01:40:58 +0530

    Renamed File

---
+++ lib/helpers/skills_lists.dart
@@ -0,0 +1,46 @@
+import 'package:portfolio_site/helpers/colors.dart';
+
+class SkillsList {
+  static List<Map<dynamic, dynamic>> skillsList = [
+    {
+      'skillName': 'Beatbox',
+      'color': CustomColors.primary,
+      'textColor': CustomColors.darkBackground,
+    },
+    {
+      'skillName': 'Chess',
+      'color': CustomColors.brightBackground,
+      'textColor': CustomColors.primary,
+    },
+    {
+      'skillName': 'Soccer',
+      'color': CustomColors.primary,
+      'textColor': CustomColors.darkBackground,
+    },
+    {
+      'skillName': 'Listening to music',
+      'color': CustomColors.brightBackground,
+      'textColor': CustomColors.primary,
+    },
+    {
+      'skillName': 'Watching movies',
+      'color': CustomColors.brightBackground,
+      'textColor': CustomColors.primary,
+    },
+    {
+      'skillName': 'Math',
+      'color': CustomColors.primary,
+      'textColor': CustomColors.darkBackground,
+    },
+    {
+      'skillName': 'Learning English',
+      'color': CustomColors.brightBackground,
+      'textColor': CustomColors.primary,
+    },
+    {
+      'skillName': 'Solving Problems',
+      'color': CustomColors.primary,
+      'textColor': CustomColors.darkBackground,
+    },
+  ];
+}
