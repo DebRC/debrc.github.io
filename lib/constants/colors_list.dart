1a5f082deb5127d36b72d9555340445e7e6917ec
DebRC <roychowdhurydebdoot@gmail.com>
2023-03-29 13:54:30 +0530

    Changed Colors

--- lib/constants/colors_list.dart
+++ lib/constants/colors_list.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class ColorsList {
-  static const Color brightBackground = Color.fromARGB(255, 91, 98, 109);
-  static const Color darkBackground = Color.fromARGB(255, 48, 52, 58);
+  static const Color brightBackground = Color.fromARGB(255, 98, 106, 128);
+  static const Color darkBackground = Color.fromARGB(255, 62, 67, 79);
   static const Color imageCircleBackground = Color(0xff28292D);
-  static const Color primary = Color(0xff6AE99F);
+  static const Color primary = Color.fromARGB(255, 132, 242, 136);
   static const Color secondary = Color(0xffDF504A);
   static const Color purple = Color(0xff5F70F9);
   static const Color gray = Color(0xffD3D3D3);
