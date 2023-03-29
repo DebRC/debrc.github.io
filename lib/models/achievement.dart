583d591d1a3bedc37df8e0c2555002fc8d673a2c
DebRC <roychowdhurydebdoot@gmail.com>
2023-03-29 17:49:28 +0530

    Added achievement Model

---
+++ lib/models/achievement.dart
@@ -0,0 +1,11 @@
+class Achievement {
+  final String title;
+  final String desc;
+  final String period;
+
+  Achievement({
+    required this.title,
+    required this.desc,
+    required this.period,
+  });
+}
