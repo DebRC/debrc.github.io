1a434b727c2812ded59c6da40252192345e1ec0c
DebRC <roychowdhurydebdoot@gmail.com>
2023-03-29 12:04:30 +0530

    Added Projects Model and Constants

---
+++ lib/helpers/projects_list.dart
@@ -0,0 +1,49 @@
+import 'package:portfolio_site/helpers/image_assets.dart';
+import 'package:portfolio_site/models/project.dart';
+
+class ProjectsList {
+  static List<Project> projectsList = [
+    Project(
+      title: 'ARTIFICIAL VISION SYSTEM FOR MEAT QUALITY GRADATION',
+      period: "2022-2023",
+      description:
+          'I’m developing android,ios and web applications using flutter platform.',
+      icon: ImageAssetConstants.flutter,
+    ),
+    Project(
+      title: 'ARTIFICIAL VISION SYSTEM FOR MEAT QUALITY GRADATION',
+      period: "2022-2023",
+      description:
+          'I’m developing android,ios and web applications using flutter platform.',
+      icon: ImageAssetConstants.flutter,
+    ),
+    Project(
+      title: 'ARTIFICIAL VISION SYSTEM FOR MEAT QUALITY GRADATION',
+      period: "2022-2023",
+      description:
+          'I’m developing android,ios and web applications using flutter platform.',
+      icon: ImageAssetConstants.flutter,
+    ),
+    Project(
+      title: 'ARTIFICIAL VISION SYSTEM FOR MEAT QUALITY GRADATION',
+      period: "2022-2023",
+      description:
+          'I’m developing android,ios and web applications using flutter platform.',
+      icon: ImageAssetConstants.flutter,
+    ),
+    Project(
+      title: 'ARTIFICIAL VISION SYSTEM FOR MEAT QUALITY GRADATION',
+      period: "2022-2023",
+      description:
+          'I’m developing android,ios and web applications using flutter platform.',
+      icon: ImageAssetConstants.flutter,
+    ),
+    Project(
+      title: 'ARTIFICIAL VISION SYSTEM FOR MEAT QUALITY GRADATION',
+      period: "2022-2023",
+      description:
+          'I’m developing android,ios and web applications using flutter platform.',
+      icon: ImageAssetConstants.flutter,
+    ),
+  ];
+}
---
+++ lib/models/project.dart
@@ -0,0 +1,13 @@
+class Project {
+  final String title;
+  final String period;
+  final String description;
+  final String icon;
+
+  Project({
+    required this.title,
+    required this.period,
+    required this.description,
+    required this.icon,
+  });
+}
